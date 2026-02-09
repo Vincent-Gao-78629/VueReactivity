e297350d23f4ecb533a9d02b3a1d3277d68aa35c
Vincent-Gao-78629 <vgao78629@gmail.com>
2026-02-09 12:32:37 -0500

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,17 @@
+<template>
+  <h1>PokeBowl Builder</h1>
+  <button @click="bowl.push('salmon')">Add Salmon</button>
+  <p>Your bowl contains: {{ bowl.join(', ') }}</p>
+</template>
+
 <script setup>
+import { ref } from 'vue'
+let bowl = ref([])
 const base = ['white rice', 'brown rice', 'salad']
 const ingredients = ['cucumber', 'edamame', 'carrots', 'seaweed salad', 'avocado']
 const protein = ['salmon', 'tuna', 'tofu', 'shrimp', 'chicken']
 const toppings = ['sesame seeds', 'green onions', 'seaweed']
 const sauces = ['soy sauce', 'spicy mayo', 'ponzu', 'eel sauce']
 </script>
-
-<template>
-  <h1>PokeBowl Builder</h1>
-</template>
 
 <style scoped></style>
