488fa46a6cccc50860d4e5c91ad5b6f7ee173005
Vincent-Gao-78629 <vgao78629@gmail.com>
2026-02-10 12:53:58 -0500

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <h1>PokeBowl Builder</h1>
-  <button @click="bowl.push('salmon')">Add Salmon</button>
-  <p>Your bowl contains: {{ bowl.join(', ') }}</p>
+
+  <button @click="console.log(bowl) .disable">iuergvgeuv</button>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-let bowl = ref([])
-const base = ['white rice', 'brown rice', 'salad']
-const ingredients = ['cucumber', 'edamame', 'carrots', 'seaweed salad', 'avocado']
-const protein = ['salmon', 'tuna', 'tofu', 'shrimp', 'chicken']
-const toppings = ['sesame seeds', 'green onions', 'seaweed']
-const sauces = ['soy sauce', 'spicy mayo', 'ponzu', 'eel sauce']
+import { ref, reactive } from 'vue'
+function addToBowl() {}
+let bowl = ref({
+  base: ['white rice'],
+  ingredients: ['cucumber', 'edamame', 'mango', 'seaweed salad', 'avocado'],
+  protein: ['salmon', 'salmon'],
+  toppings: ['crispy onions'],
+  sauces: ['spicy mayo', 'eel sauce'],
+})
+const items = {
+  base: ['white rice', 'brown rice', 'salad'],
+  ingredients: ['cucumber', 'edamame', 'mango', 'seaweed salad', 'avocado'],
+  protein: ['salmon', 'tuna', 'tofu', 'shrimp', 'chicken'],
+  toppings: ['sesame seeds', 'green onions', 'seaweed', 'crispy onions', 'fried garlic'],
+  sauces: ['soy sauce', 'spicy mayo', 'ponzu', 'eel sauce', 'wasabi mayo'],
+}
 </script>
 
 <style scoped></style>
