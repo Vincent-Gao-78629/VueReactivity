da2532bf9162e7e59a21b6d9d5357cd5f3f02e7d
Vincent-Gao-78629 <vgao78629@gmail.com>
2026-02-13 14:13:34 -0500

    just hope

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <h1>PokeBowl Builder</h1>
-
-  <button @click="console.log(bowl) .disable">iuergvgeuv</button>
+  <h1>Calculator</h1>
+  <div>
+    <button v-for="number in numbers">{{ number }}</button>
+    <button>+</button>
+    <button>-</button>
+    <button>x</button>
+    <button>/</button>
+  </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue'
-function addToBowl() {}
-let bowl = ref({
-  base: ['white rice'],
-  ingredients: ['cucumber', 'edamame', 'mango', 'seaweed salad', 'avocado'],
-  protein: ['salmon', 'salmon'],
-  toppings: ['crispy onions'],
-  sauces: ['spicy mayo', 'eel sauce'],
-})
-const items = {
-  base: ['white rice', 'brown rice', 'salad'],
-  ingredients: ['cucumber', 'edamame', 'mango', 'seaweed salad', 'avocado'],
-  protein: ['salmon', 'tuna', 'tofu', 'shrimp', 'chicken'],
-  toppings: ['sesame seeds', 'green onions', 'seaweed', 'crispy onions', 'fried garlic'],
-  sauces: ['soy sauce', 'spicy mayo', 'ponzu', 'eel sauce', 'wasabi mayo'],
-}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let number1
+let number2
+const history = []
 </script>
 
 <style scoped></style>
