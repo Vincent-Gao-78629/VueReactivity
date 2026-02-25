96fbcea0149a97aea209be77c9094a17a8d18e9d
Vincent-Gao-78629 <vgao78629@gmail.com>
2026-02-25 04:18:24 -0500

    me+cs=error

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,19 +1,76 @@
 <template>
   <h1>Calculator</h1>
-  <div>
-    <button v-for="number in numbers">{{ number }}</button>
-    <button>+</button>
-    <button>-</button>
-    <button>x</button>
-    <button>/</button>
-  </div>
+  <button v-for="number in numbers" :key="number" @click="selectNumber(number)">
+    {{ number }}
+  </button>
+  <button @click="selectOperator('+')">+</button>
+  <button @click="selectOperator('-')">-</button>
+  <button @click="selectOperator('*')">*</button>
+  <button @click="selectOperator('/')">/</button>
+  <button @click="calculate">=</button>
+  <button @click="clear">Clear</button>
+  <div>{{ number1 }} {{ operator }} {{ number2 }} = {{ result }}</div>
 </template>
 
 <script setup>
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-let number1
-let number2
-const history = []
+import { ref } from 'vue'
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const number1 = ref(null)
+const number2 = ref(null)
+const operator = ref(null)
+const result = ref(null)
+
+function selectNumber(number) {
+  if (number1.value === null) {
+    number1.value = number
+  } else {
+    number2.value = number
+  }
+}
+function selectOperator(op) {
+  operator.value = op
+}
+function add() {
+  result.value = number1.value + number2.value
+}
+function subtract() {
+  result.value = number1.value - number2.value
+}
+function multiply() {
+  result.value = number1.value * number2.value
+}
+function divide() {
+  if (number2.value !== 0) {
+    result.value = number1.value / number2.value
+  } else {
+    result.value = 'Error: Division by zero'
+  }
+}
+
+function calculate() {
+  if (operator.value === '+') {
+    add()
+  } else if (operator.value === '-') {
+    subtract()
+  } else if (operator.value === '*') {
+    multiply()
+  } else if (operator.value === '/') {
+    divide()
+  }
+}
+function clear() {
+  number1.value = null
+  number2.value = null
+  operator.value = null
+  result.value = null
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+button {
+  margin: 2px;
+  padding: 10px;
+  font-size: 18px;
+}
+</style>
