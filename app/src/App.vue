bd2f5736513b173ebff964e9e089109a9d3ebcd8
Vincent-Gao-78629 <vgao78629@gmail.com>
2026-03-11 03:46:29 -0400

    lock

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,26 +4,28 @@
     <button v-for="number in numbers" :key="number" @click="selectNumber(number)">
       {{ number }}
     </button>
-    <button @click="calculate">=</button>
-    <!-- make calc stop selection -->
 
     <button @click="selectOperator('+')">+</button>
     <button @click="selectOperator('-')">-</button>
     <button @click="selectOperator('*')">*</button>
     <button @click="selectOperator('/')">/</button>
-    <button @click="removeLast">CE</button>
-    <!-- bad -->
-    <button @click="negate">(−)</button>
-    <!-- bad -->
 
+    <button @click="negate">±</button>
+    <button @click="removeLast">CE</button>
     <button @click="clear">C</button>
+    <button @click="calculate">=</button>
   </div>
 
-  <div>{{ number1 }} {{ operator }} {{ number2 }} = {{ result }}</div>
+  <div>{{ equation }}</div>
+
+  <div>
+    <h2>History</h2>
+    <li v-for="(calc, index) in history" :key="index">{{ calc }}</li>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 const input = ref('')
@@ -32,62 +34,68 @@ const number2 = ref(null)
 const operator = ref(null)
 const result = ref(null)
 
+const history = ref([])
+
+const equation = computed(() => {
+  if (result.value !== null) {
+    return `${number1.value ?? ''} ${operator.value ?? ''} ${number2.value ?? ''} = ${result.value}`
+  }
+  return `${number1.value ?? ''} ${operator.value ?? ''} ${input.value ?? ''}`
+})
+
 function selectNumber(number) {
-  if (operator.value === null) {
-    input.value += number
-    number1.value = Number(input.value)
-  } else {
-    input.value += number
-    number2.value = Number(input.value)
-  }
+  input.value += number
 }
+
 function selectOperator(op) {
-  if (number1.value != null || number2.value === null) {
-    // make so if have nnumber2, op cant be seelc, after calc, n2 cant seelct
-    operator.value = opt
+  if (operator.value === null && input.value !== '') {
+    number1.value = Number(input.value)
+    operator.value = op
     input.value = ''
-  }
-}
-function add() {
-  result.value = number1.value + number2.value
-}
-function subtract() {
-  result.value = number1.value - number2.value
-}
-function multiply() {
-  result.value = number1.value * number2.value
-}
-
-function divide() {
-  if (number2.value != 0) {
-    result.value = number1.value / number2.value
-  } else {
-    result.value = 'Error: Division by zero'
-  }
-}
-function removeLast(number1, number2) {}
-
-function negate(number1, number2) {
-  if (operator.value === null) {
-    input.value += number
-    number1.value = -1 * Number(input.value)
-  } else {
-    input.value += number
-    number2.value = -1 * Number(input.value)
+  } else if (result.value !== null) {
+    number1.value = result.value
+    operator.value = op
+    input.value = ''
+    result.value = null
   }
 }
 
 function calculate() {
-  if (operator.value === '+') {
-    add()
-  } else if (operator.value === '-') {
-    subtract()
-  } else if (operator.value === '*') {
-    multiply()
-  } else if (operator.value === '/') {
-    divide()
+  if (operator.value && input.value !== '') {
+    number2.value = Number(input.value)
+
+    if (operator.value === '+') {
+      result.value = number1.value + number2.value
+    } else if (operator.value === '-') {
+      result.value = number1.value - number2.value
+    } else if (operator.value === '*') {
+      result.value = number1.value * number2.value
+    } else if (operator.value === '/') {
+      if (number2.value != 0) {
+        result.value = number1.value / number2.value
+      } else {
+        result.value = 'Error: Division by zero'
+      }
+    }
+    history.value.unshift(`${number1.value} ${operator.value} ${number2.value} = ${result.value}`)
+
+    input.value = ''
   }
 }
+function removeLast() {
+  input.value = input.value.slice(0, -1)
+}
+
+function negate() {
+  if (input.value !== '') {
+    if (input.value.startsWith('-')) {
+      input.value = input.value.slice(1)
+    } else {
+      input.value = '-' + input.value
+    }
+  }
+}
+
 function clear() {
   input.value = ''
   number1.value = null
@@ -99,15 +107,20 @@ function clear() {
 
 <style scoped>
 #calcButtons {
-  flex: auto;
-  max-width: 15%;
+  display: grid;
+  grid-template-columns: repeat(3, 50px);
+  gap: 5px;
+  margin-top: 10px;
 }
 
 button {
-  margin: 2px;
   height: 50px;
-  width: 50px;
-  padding: 1px;
   font-size: 18px;
+  cursor: pointer;
+}
+
+div {
+  margin-top: 20px;
+  font-size: 24px;
 }
 </style>
